02701aabe0dc74612713a9520bb8d81b0dc0789e
ColacX Unknown <ColacX@hotmail.com>
2011-11-10 23:51:43 +0100

    structured up some of the basic logic and relations between components

---
+++ TextData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NotepadCS
+{
+	class TextData
+	{
+		public bool Modified = false;
+		List<string> listData = new List<string>();
+
+		public void LoadFrom( string filepath )
+		{
+			//todo thread this operation may take a lot of time and it looks cool when it loads
+			foreach( var line in File.ReadLines( filepath, Encoding.ASCII ) )
+			{
+				listData.Add( line );
+			}
+		}
+
+		public void SaveAs( string filepath )
+		{
+		}
+
+		//update all listeners
+
+		//row and column wise data manipulation
+	}
+}
