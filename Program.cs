24ee1ef8f3c83996b6ce61469cc8ac5ccf4d69f7
ColacX Unknown <ColacX@hotmail.com>
2011-11-10 23:35:44 +0100

    starting project NotepadCS

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NotepadCS
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main( string[] args )
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault( false );
+
+			//todo parse args
+			Application.Run( new MainForm() );
+		}
+	}
+}
